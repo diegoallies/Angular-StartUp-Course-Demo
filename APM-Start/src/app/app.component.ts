0cf5e0af7598419ab9c1d545389439a14a08832a
Diego Allies <diego.allies@arealytics.com.au>
2023-02-27 13:22:37 +0200

    new

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Angular: Getting Started';
-}
+export class AppComponent
