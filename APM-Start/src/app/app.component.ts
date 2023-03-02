48a045dec028fe5564cdb676edff5a9bbbf8122d
Diego Allies <diego.allies@arealytics.com.au>
2023-03-02 12:58:20 +0200

    push coide

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,1 +1,14 @@
-export class AppComponent
+import { Component } from '@angular/core';
+
+@Component({
+ selector: 'pm-root',
+ template: `
+  <div><h1>{{pageTitle}}</h1>
+    <pm-products> </pm-products>
+  </div>
+ `
+})
+
+export class AppComponent {
+  pageTitle: string = 'Encrypto Product Management';
+}
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
